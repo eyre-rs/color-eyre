0545cd4c523042ae4a62fca3acd87537133658e9
Jane Lusby <jlusby@yaah.dev>
2020-05-17 10:54:43 -0700

    fix backslash for windows

--- tests/minimal.rs
+++ tests/minimal.rs
@@ -51,7 +51,7 @@ fn read_config() -> Result<(), Report> {
 }
 
 // Define at the bottom to prevent it from changing line numbers
-#[cfg(feature = "capture-spantrace")]
+#[cfg(all(feature = "capture-spantrace", not(windows)))]
 static EXPECTED: &str = "Error: 
    0: \u{1b}[31mUnable to read config\u{1b}[0m
    1: \u{1b}[31mNo such file or directory (os error 2)\u{1b}[0m
@@ -69,5 +69,20 @@ static EXPECTED: &str = "Error:
 static EXPECTED: &str = "Error: 
    0: \u{1b}[31mUnable to read config\u{1b}[0m
    1: \u{1b}[31mNo such file or directory (os error 2)\u{1b}[0m
+
+\u{1b}[36mSuggestion\u{1b}[0m: try using a file that exists next time";
+
+// Define at the bottom to prevent it from changing line numbers
+#[cfg(all(feature = "capture-spantrace", windows))]
+static EXPECTED: &str = "Error: 
+   0: \u{1b}[31mUnable to read config\u{1b}[0m
+   1: \u{1b}[31mNo such file or directory (os error 2)\u{1b}[0m
+
+  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SPANTRACE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  
+   0: \u{1b}[31mminimal\u{1b}[0m\u{1b}[31m::\u{1b}[0m\u{1b}[31mread_file\u{1b}[0m with \u{1b}[36mpath=\"fake_file\"\u{1b}[0m
+      at tests\\minimal.rs:40
+   1: \u{1b}[31mminimal\u{1b}[0m\u{1b}[31m::\u{1b}[0m\u{1b}[31mread_config\u{1b}[0m
+      at tests\\minimal.rs:46
 
 \u{1b}[36mSuggestion\u{1b}[0m: try using a file that exists next time";
