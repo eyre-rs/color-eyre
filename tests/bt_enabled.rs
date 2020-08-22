5436177c75ce08dccbbddd5552f18d54be5f587e
Jane Lusby <jlusby@yaah.dev>
2020-08-22 10:46:16 -0700

    fix tests

--- tests/bt_enabled.rs
+++ tests/bt_enabled.rs
@@ -2,14 +2,14 @@ use color_eyre::eyre;
 use eyre::eyre;
 
 #[test]
-fn disabled() {
+fn enabled() {
     color_eyre::config::HookBuilder::default()
-        .display_env_section(false)
+        .display_env_section(true)
         .install()
         .unwrap();
 
     let report = eyre!("error occured");
 
     let report = format!("{:?}", report);
-    assert!(!report.contains("RUST_BACKTRACE"));
+    assert!(report.contains("RUST_BACKTRACE"));
 }
